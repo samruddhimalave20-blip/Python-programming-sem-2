d04cb4abc7bf25634ec0078e4f39d6289781b1f5
samruddhimalave20-blip <samruddhimalave20@gmail.com>
2026-02-03 11:15:24 +0530

    Create Exp1Act.py

---
+++ Exp1/Exp1Act.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter First Number:"))
+num2 = int(input("Enter Second Number:")) 
+
+sum=num1+num2
+print("Addition=",sum) 
+
+
+
+
+sub=num1-num2 
+print("Substraction=",sub) 
+
+
+mul=num1*num2
+print("Multiplication=",mul)
+
+div=num1/num2
+print("Division=",div)
