ea2aa05b6e3991eb1a4215aff88502a9ed4bcf24
samruddhimalave20-blip <samruddhimalave20@gmail.com>
2026-02-17 11:04:48 +0530

    Create Expt3.act.py

---
+++ Expt3/Expt3.act.py
@@ -0,0 +1,12 @@
+# program to print multiplication table of 10
+"""
+Created on Tue Feb 17 10:54:15 2026
+
+@author: samruddhi malave 
+"""
+
+for i in range (1, 11):
+    print("Multiplication Table of",i)
+    for j in range(1, 11):
+        print(i, "X", j, "=", i * j)
+    print()
